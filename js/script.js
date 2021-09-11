dc32374f41bed0a2845dd82739f9a59c290e687c
Arreaum <73148019+arriaoedu123@users.noreply.github.com>
2021-09-11 17:49:36 -0300

    Update files

--- js/script.js
+++ js/script.js
@@ -2,17 +2,28 @@
 # Creator: Arreaum
 # GitHub: https://github.com/arriaoedu123/
 # Creation date: 21/08/2021
-# Version: 1.0
+# Upload date: 11/09/2021
+# Version: 1.2
 */
 
-window.addEventListener("scroll", function(){
-  const header = document.querySelector("header");
-  header.classList.toggle('sticky', window.scrollY > 0);
-});
+const header = document.querySelector("header")
+const menu = document.querySelector('.menu')
+const menuBtn = document.querySelector('.menu-btn')
+const closeBtn = document.querySelector('.close-btn')
+const scrollLine = document.querySelector(".scroll-line-div")
+const cardSection = document.querySelector(".section-one")
 
-const menu = document.querySelector('.menu');
-const menuBtn = document.querySelector('.menu-btn');
-const closeBtn = document.querySelector('.close-btn');
+if (window.scrollY > 0) {
+  header.classList.toggle('sticky')
+  scrollLine.classList.toggle('animate')
+} else
+if (window.scrollY >= 306) { cardSection.classList.add('anim-card') }
+
+window.addEventListener("scroll", function(){
+  header.classList.toggle('sticky', window.scrollY > 0);
+  scrollLine.classList.toggle('animate', window.scrollY > 0);
+  cardSection.classList.add('anim-card', window.scrollY >= 306)
+});
 
 menuBtn.addEventListener("click", () => {
   menu.classList.add('active');
